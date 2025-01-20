9e9a253fef8eb306609a6b13148e5da66b0af14a
Akansha <akanshamore21@gmail.com>
2025-01-20 22:01:49 +0530

    created PetListings, Notifications and Adoption form routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,79 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
+import NavBar from "./components/NavBar";
+import PetCard from "./components/PetCard";
+
+const PetListingsPage = () => {
+  return (
+    <div>
+      <PetCard />
+    </div>
+  );
+};
+
+const AdoptionFormPage = () => {
+  return (
+    <div>
+      <h1>Adoption Form</h1>
+    </div>
+  );
+};
+
+const NotificationsPage = () => {
+  return (
+    <div>
+      <h1>Notifications</h1>
+    </div>
+  );
+};
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <NavBar />
+      {children}
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/pet-listings"
+        element={
+          <Layout>
+            <PetListingsPage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/notifications"
+        element={
+          <Layout>
+            <NotificationsPage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/adoption-form"
+        element={
+          <Layout>
+            <AdoptionFormPage />
+          </Layout>
+        }
+      />
+    </Routes>
   );
 }
 
